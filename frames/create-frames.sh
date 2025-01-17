380fc17e5ca557b2816a419b79ea380a3768ed43
esdmr <esdmr0@gmail.com>
2025-01-17 17:47:44 +0330

    Clear old frames in create-frames

--- frames/create-frames.sh
+++ frames/create-frames.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+rm -rf out
 mkdir -p out
 ffmpeg -i src/badapple.mp4 -filter:v "scale=$(sed '1q;d' options.txt):$(sed '2q;d' options.txt)" -r "$(sed '3q;d' options.txt)" out/%04d.png
 date >out/build.txt
